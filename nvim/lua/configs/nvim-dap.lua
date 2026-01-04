2dac075f083fa2381d6d078c164b20d5b2ffad0e
AaronH_MSI <a_huinink@hotmail.ca>
2026-01-04 08:47:21 -0800

    dap config

--- nvim/lua/configs/nvim-dap.lua
+++ nvim/lua/configs/nvim-dap.lua
@@ -2,38 +2,43 @@ local dap = require("dap")
 
 -- Use Windows lldb.exe
 dap.adapters.codelldb = {
-    name = "Windows LLDB",
     type = "server",
-    host = "localhost",
-    port = "8081",
-    -- executable = {
-    --     command = "/mnt/d/repos/codelldb/extension/adapter/codelldb.exe",
-    --     args = {
-    --         "--port",
-    --         "${port}",
-    --     },
-    --     detached = false,
-    -- },
+    port = "${port}",
+    executable = {
+        command = "/home/aaron/tools/codelldb/extension/adapter/codelldb",
+        args = {
+            "--port",
+            "${port}",
+        },
+    },
 }
-
-local convert_to_winpath = function(wsl_path)
-    local output_handle = io.popen("wslpath -w " .. wsl_path)
-    local win_path = output_handle:read()
-    output_handle:close()
-    return win_path
-end
 
 dap.configurations.cpp = {
     {
-        name = "Debug JUCE plugin in AudioPluginHost",
+        name = "Remote Windows debug JUCE plugin in AudioPluginHost",
         type = "codelldb",
         request = "launch",
-        program = convert_to_winpath("/mnt/d/repos/JUCE/extras/AudioPluginHost/Builds/VisualStudio2022/x64/Debug/App/AudioPluginHost.exe"),
-        cwd = convert_to_winpath("${workspaceFolder}"),
+        cwd = "D:\\plugins\\debug",
+        program = "D:\\plugins\\debug\\AudioPluginHost\\AudioPluginHost.exe",
+        initCommands = {
+            -- function()
+            --     local plugin_name = vim.fn.input("Enter the plugin name: ")
+            --     return "settings set target.debug-file-search-paths ~/repos/" .. plugin_name .. "/Builds/VisualStudio2022/x64/Debug/App"
+            -- end,
+            "platform select remote-windows",
+            "platform connect connect://localhost:8080",
+            "platform settings -w D:\\plugins\\debug",
+        },
+        preRunCommands = {
+            -- function() return "target module add " .. vim.fn.input("Enter path to JUCE plugin: ", vim.fn.getcwd() .. "/Builds/") end,
+            "target module add /home/aaron/repos/SimpleEQLinux/Builds/VisualStudio2022/x64/Debug/VST3/SimpleEQLinux.vst3/Contents/x86_64-win/SimpleEQLinux.vst3",
+            "target symbols add /home/aaron/repos/SimpleEQLinux/Builds/VisualStudio2022/x64/Debug/VST3/SimpleEQLinux.pdb",
+            "target symbols add /home/aaron/JUCE/extras/AudioPluginHost/Builds/VisualStudio2022/x64/Debug/App/AudioPluginHost.pdb",
+        },
+        exitCommands = {
+            "platform disconnect",
+        },
         stopOnEntry = false,
-        sourceMap = {
-            ["D:\\repos\\"] = "/mnt/d/",
-        }
     },
     {
         name = "Debug Windows .exe",
